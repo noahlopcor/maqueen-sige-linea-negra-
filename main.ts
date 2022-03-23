2654423ed6be9eb19631b57bd1058061ad3f79fa
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-03-23 09:27:02 +0100

    holiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 basic.forever(function () {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    } else {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    }
+})
+basic.forever(function () {
     if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
     } else {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
     }
-    if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1) {
+    if (maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
     } else {
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
